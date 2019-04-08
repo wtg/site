22371f4713e6f2b5f97ce7cb68ebc7ca68ccf1ae
Joseph Lyon <joeylyon1@gmail.com>
2019-04-08 12:46:12 -0400

    add webpack vue project

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default Vue.extend({
+  name: 'WebTech',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
